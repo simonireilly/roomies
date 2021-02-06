b770421cbda7bfb1222c5fd701f69f84e2124ab3
simon <simonireilly@gmail.com>
2021-02-06 09:17:12 +0000

    Merge master, resolve conflicts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { usePresence } from '@roomservice/react'
 import { useEffect, useState } from 'react'
+import { Controls } from '../components/games/controls'
 import Opponent from '../components/games/opponent'
 
 export type Directions = {
@@ -24,38 +25,6 @@ const directions: Directions = {
 
 export default function Home() {
   const [players, setMyPlayer] = usePresence<Player>("demo", "players");
-
-  // On boot connect to the room and load all presences
-  useEffect(() => {
-    async function load() {
-      const rs = new RoomService({
-        auth: '/api/roomservice'
-      })
-
-      // Set presence in the client side state to be the current presence
-      const room = await rs.room('demo')
-      const p = room.presence()
-      setPresence(p)
-
-      // Set the initial positions
-      const v = await p.getAll<Player>('players')
-      setPositions(v)
-
-      // Subscribe any updates to the room positions
-      return room.subscribe<Player>(p, 'players', (msg) => {
-        setPositions(msg)
-      })
-    }
-
-    load().catch(console.error)
-  }, [])
-
-  const directions: Directions = {
-    ArrowUp: false,
-    ArrowDown: false,
-    ArrowRight: false,
-    ArrowLeft: false
-  }
 
   const boxWidth = 3;
 
@@ -130,38 +99,6 @@ export default function Home() {
             max="50"
             onBlur={(e) => setGameSpeed(e.target.valueAsNumber)} />
         </label>
-      </div>
-      <div className="board">
-        <label>
-          UserName: &nbsp;
-          <input onChange={(e) => setName(e.target.value)} />
-        </label>
-        <hr />
-        <br />
-        <div id="pane">
-          <div
-            id="box"
-            className="player"
-            style={{ left, top }}
-            title="you"
-          ></div>
-          {presence && Object.keys(positions)
-            .filter(userName => userName !== presence.me)
-            .map(userName => (
-              <div
-                key={userName}
-                title={positions[userName].name}
-                className="opponent"
-                style={{
-                  left: positions[userName].x,
-                  top: positions[userName].y
-                }}>
-                <span className="pill">
-                  {positions[userName].name}
-                </span>
-              </div>
-            ))}
-        </div>
       </div>
       <div className="controls">
         <Controls directions={directions} />
