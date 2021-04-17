332562681939cd6bac7bc5a21315ad900357a09b
Simon Reilly <simon.reilly@infinityworks.com>
2021-04-17 17:14:37 +0100

    Add responsive viewport and set header title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
 import { RoomServiceProvider } from "@roomservice/react";
+import Head from 'next/head'
 
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <RoomServiceProvider clientParameters={{ auth: "/api/roomservice" }}>
+      <Head>
+        <title>ROOMIES!!!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
     <Component {...pageProps} />
   </RoomServiceProvider>
 }
